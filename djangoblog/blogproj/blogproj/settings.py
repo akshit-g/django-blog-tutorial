3bfa09173d07b16a7eb89f17bc501ce5c55dd2bb
Akshit Gandotra <90208584+akshit-g@users.noreply.github.com>
2023-06-23 23:38:19 +0530

    Update settings.py

--- djangoblog/blogproj/blogproj/settings.py
+++ djangoblog/blogproj/blogproj/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-t!!3s!qytdejsk+t#jyll+ypl=(9s^^*v7v=rc+8a*o)o(dfuj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
